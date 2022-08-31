f343bea51c0ee7a7287f23bcffbcb81e1f26a317
Chekube <mdankaro@gmail.com>
2022-08-31 03:15:21 +0100

    time

---
+++ time.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="time.css">
+    <title>Time</title>
+</head>
+<body>
+<h1>Date & Time</h1>
+   <p1 id="date"><?php echo date("m/d/Y");?></p1> 
+   <p id="time"><?php echo date("h:i:s A");?></p>
+</body>
+</html>

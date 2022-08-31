91536bc0f84c82fa5ae56c02e998dc2d1910de9c
Chekube <mdankaro@gmail.com>
2022-08-31 03:29:52 +0100

    add

--- time.php
+++ time.php
@@ -10,6 +10,6 @@
 <body>
 <h1>Date & Time</h1>
    <p1 id="date"><?php echo date("m/d/Y");?></p1> 
-   <p id="time"><?php echo date("h:i:s A");?></p>
+   <p2 id="time"><?php echo date("h:i:s A");?></p2>
 </body>
 </html>
